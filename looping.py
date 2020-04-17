09408c2f0dd15c4cc4df6522c2a4551f39fdf940
fesasabil <kijee212@gmail.com>
2020-04-17 13:26:42 +0700

    fix: looping

--- looping.py
+++ looping.py
@@ -1,3 +1,12 @@
+# What is Loop?
+# Loops can execute a block of code number of times until a certain condition is met. Their usage is fairly common in programming. Unlike other programming language that have For Loop, while loop, dowhile, etc.
+
+# What is For Loop?
+# For loop is used to iterate over elements of a sequence. It is often used when you have a piece of code which you want to repeat "n" number of time.
+
+# What is While Loop?
+# While Loop is used to repeat a block of code. Instead of running the code block once, It executes the code block multiple times until a certain condition is met.
+
 def main():
     x, y = 8, 9
 
@@ -72,3 +81,35 @@ for i in range(1,6):
         for j in range(i):
             print("*",end=' ')
         print()
+
+
+# While
+x = 0
+# define a while loop
+while(x < 4):
+    print(x)
+    x = x+1
+
+for x in range(2, 7):
+    print(x)
+
+# use a for loop over a collection
+Months = ["Jan","Feb","Mar","Apr","May","June"]
+for m in Months:
+    print(m)
+
+# use the break and continue statements
+for x in range(10,20):
+    # if (x == 15): break
+    if (x % 2 == 0) : continue
+    print(x)
+
+
+# How to use "enumerate" function for "For Loop"
+Months = ["Jan","Feb","Mar","Apr","May","June"]
+for i, m in enumerate (Months):
+    print(i,m)
+
+
+for i in '123':
+    print("qodr", i)
